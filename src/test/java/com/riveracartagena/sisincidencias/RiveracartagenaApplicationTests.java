79e7db2770963aca202838e301c5648809058568
Angig Rivera <angiggianellar@gmail.com>
2023-07-21 22:31:05 -0500

    version 1

---
+++ src/test/java/com/riveracartagena/sisincidencias/RiveracartagenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riveracartagena.sisincidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiveracartagenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
